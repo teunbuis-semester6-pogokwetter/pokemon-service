c524ea742cc7cfe5cb95596b0b94800449127ab3
Teun <teunbuis17@gmail.com>
2023-05-20 18:35:18 +0200

    Update

--- src/main/java/com/teun/pokemonservice/PokemonServiceApplication.java
+++ src/main/java/com/teun/pokemonservice/PokemonServiceApplication.java
@@ -1,6 +1,5 @@
 package com.teun.pokemonservice;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -10,9 +9,6 @@ import org.springframework.cache.annotation.EnableCaching;
 public class PokemonServiceApplication {
 
 	public static void main(String[] args) {
-
-		Dotenv dotenv = Dotenv.load();
-		dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
 		SpringApplication.run(PokemonServiceApplication.class, args);
 	}
 
